a86a53b31f1f90739b5f1a653f20a390358d4a65
Ciciovan Bogdan <bogdigamer55@gmail.com>
2021-12-03 08:44:51 +0200

    a lot of changes

--- merch/de_la_curs.py
+++
@@ -1,5 +0,0 @@
-# def navbar_data_provider():
-# # numeleapp.context_proc.nume_cont_proc in settings
-#     open_cart = get_open_cart()
-#     cart_items = CartItems.objects.filter().count()
-#     return {'cart_items_context': cart_items}
